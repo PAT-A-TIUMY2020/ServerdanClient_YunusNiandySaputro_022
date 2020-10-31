02171fb569f11f0eeef4952fb2fb4a3be4c18595
ReAlpha39 <yunusw718@gmail.com>
2020-10-31 15:51:49 +0800

    Add project files.

---
+++ Client_YunusNiandySaputro_022/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_YunusNiandySaputro_022
+{
+    public partial class Form1 : Form
+    {
+
+        ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+        ServiceReference1.Koordinat a = new ServiceReference1.Koordinat();
+        ServiceReference1.Koordinat b = new ServiceReference1.Koordinat();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int val1 = Int32.Parse(textBox1.Text);
+                int val2 = Int32.Parse(textBox2.Text);
+
+                label7.Text = obj.Tambah(val1, val2);
+                label8.Text = obj.Kurang(val1, val2);
+                label9.Text = obj.Kali(val1, val2);
+                label10.Text = obj.Bagi(val1, val2);
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Server_YunusNiandySaputro_022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_YunusNiandySaputro_022
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
